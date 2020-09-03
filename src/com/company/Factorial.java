e1bca09ff9b9803e132fc73e6d4e6ddbad0ff245
Ryuo-Rei <70551513+Ryuo-Rei@users.noreply.github.com>
2020-09-03 16:43:57 +0900

    階乗計算追加

---
+++ src/com/company/Factorial.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.Scanner;
+
+import static java.lang.System.exit;
+
+// 2020/09/03/ 15:50 ~
+public class Factorial {
+
+    public void CalculateFactorial() {
+        boolean isError = true;
+        // long factorial = 1;
+
+        while(true) {
+            if(isError = true) {
+                System.out.println("数値を入力してください：");
+            }
+            if(isError = false) {
+                System.out.println("数値(0 ~ 99)で入力してください。");
+            }
+
+            Scanner scan = new Scanner(System.in);
+            String input = scan.nextLine();
+
+            if (input.equalsIgnoreCase("end")) {
+                exit(0);
+            }
+
+            if (isNumber(input) == false) {
+                isError = false;
+                continue;
+            }
+            isError = true;
+
+            long factorial = 1;
+
+            for(int i = Integer.parseInt(input); 1 <= i; i--) {
+                factorial *= i;
+            }
+            System.out.println(factorial);
+        }
+    }
+
+    private boolean isNumber(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        }
+        catch(Exception e) {
+            return false;
+        }
+    }
+}
---
+++ src/com/company/FactorialMain.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.List;
+
+public class FactorialMain {
+
+    public static void main(String[] args) {
+        // write your code here
+        Factorial factorial = new Factorial();
+        factorial.CalculateFactorial();
+    }
+}
