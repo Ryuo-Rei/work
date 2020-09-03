10e2e8cf3b69834067e3ca60e60166a2e151e7bf
Ryuo-Rei <70551513+Ryuo-Rei@users.noreply.github.com>
2020-09-03 09:44:43 +0900

    99表修正

--- src/com/company/MultiplicationTable.java
+++ src/com/company/MultiplicationTable.java
@@ -6,8 +6,10 @@ public class MultiplicationTable {
     public void CreateMultiplicationTable() {
 
         int[][] numberArray = new int[9][9];
+        System.out.println("   1  2  3  4  5  6  7  8  9");
         // 99を表示するための2重ループ
         for (int i = 1; i <= 9; i++) {
+            System.out.print(i + " ");
             for (int j = 1; j <= 9; j++) {
                 int sum = i * j;
                 if (j == 9) {
