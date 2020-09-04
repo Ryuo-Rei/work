5696141c341e0e57c284604d9a871b01cbe76f88
Ryuo-Rei <70551513+Ryuo-Rei@users.noreply.github.com>
2020-09-04 16:19:31 +0900

    分を時間や日に変換　修正

--- src/com/company/MinutesToTimeAndDay.java
+++ src/com/company/MinutesToTimeAndDay.java
@@ -29,32 +29,25 @@ public class MinutesToTimeAndDay {
         long hours = Long.parseLong(String.format("%.0f", Math.floor(minute / 60)));
         minute -= (hours * 60);
 
+
+        System.out.println("入力値 " + minutes);
         if (days > 0) {
+            System.out.print("出力：" + days + "日");
             if (hours > 0 && minute > 0) {
-                System.out.println("入力値 " + minutes);
-                System.out.println("出力：" + days + "日" + hours + "時間" + minute + "分");
+                System.out.println(" " + hours + "時間" + minute + "分");
             } else if (hours > 0 && minute == 0) {
-                System.out.println("入力値 " + minutes);
-                System.out.println("出力：" + days + "日" + hours + "時間");
+                System.out.println(" " + hours + "時間");
             } else if (hours == 0 && minute > 0) {
-                System.out.println("入力値 " + minutes);
-                System.out.println("出力：" + days + "日" + minute + "分");
-            } else {
-                System.out.println("入力値 " + minutes);
-                System.out.println("出力：" + days + "日");
+                System.out.println(" " + minute + "分");
             }
         } else {
             if (hours > 0 && minute > 0) {
-                System.out.println("入力値 " + minutes);
                 System.out.println("出力：" + hours + "時間" + minute + "分");
             } else if (hours > 0 && minute == 0) {
-                System.out.println("入力値 " + minutes);
                 System.out.println("出力：" + hours + "時間");
             } else if (hours == 0 && minute > 0) {
-                System.out.println("入力値 " + minutes);
                 System.out.println("出力：" + minute + "分");
             } else {
-                System.out.println("入力値 " + minutes);
                 System.out.println("出力：" + minute + "分");
             }
         }
