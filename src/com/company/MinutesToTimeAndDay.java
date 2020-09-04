9af18a6cf27dcb50e2c724b7674250e5facc2e2d
Ryuo-Rei <70551513+Ryuo-Rei@users.noreply.github.com>
2020-09-04 14:20:40 +0900

    分を時間や日に変換 追加

---
+++ src/com/company/MinutesToTimeAndDay.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.Scanner;
+
+// 2020/09/04 11:30 ~14:15
+public class MinutesToTimeAndDay {
+
+    public void ConvertMinute() {
+        try {
+            Scanner scan = new Scanner(System.in);
+            long minutes = scan.nextLong();
+
+            while (minutes < 0 || 100000 <= minutes) {
+                System.out.println("値は0 ~ 99999の整数で入力してください。");
+                continue;
+            }
+
+            CalcurateTimes(minutes);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void CalcurateTimes(long minutes) {
+        long minute = minutes;
+        long days = Long.parseLong(String.format("%.0f", Math.floor(minutes / 1440)));
+        minute -= (days * 1440);
+        long hours = Long.parseLong(String.format("%.0f", Math.floor(minute / 60)));
+        minute -= (hours * 60);
+
+        if (days > 0) {
+            if (hours > 0 && minute > 0) {
+                System.out.println("入力値 " + minutes);
+                System.out.println("出力：" + days + "日" + hours + "時間" + minute + "分");
+            } else if (hours > 0 && minute == 0) {
+                System.out.println("入力値 " + minutes);
+                System.out.println("出力：" + days + "日" + hours + "時間");
+            } else if (hours == 0 && minute > 0) {
+                System.out.println("入力値 " + minutes);
+                System.out.println("出力：" + days + "日" + minute + "分");
+            } else {
+                System.out.println("入力値 " + minutes);
+                System.out.println("出力：" + days + "日");
+            }
+        } else {
+            if (hours > 0 && minute > 0) {
+                System.out.println("入力値 " + minutes);
+                System.out.println("出力：" + hours + "時間" + minute + "分");
+            } else if (hours > 0 && minute == 0) {
+                System.out.println("入力値 " + minutes);
+                System.out.println("出力：" + hours + "時間");
+            } else if (hours == 0 && minute > 0) {
+                System.out.println("入力値 " + minutes);
+                System.out.println("出力：" + minute + "分");
+            } else {
+                System.out.println("入力値 " + minutes);
+                System.out.println("出力：" + minute + "分");
+            }
+        }
+    }
+
+}
---
+++ src/com/company/MinutesToTimeAndDayMain.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class MinutesToTimeAndDayMain {
+
+    public static void main(String[] args) {
+        MinutesToTimeAndDay mTTAD = new MinutesToTimeAndDay();
+        mTTAD.ConvertMinute();
+    }
+}
